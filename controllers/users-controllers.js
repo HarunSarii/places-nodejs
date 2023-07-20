19a4d7406d672a73279ef015499fe5f907019076
HarunSarii <harunsari14@gmail.com>
2023-07-20 19:29:04 +0300

    feat: add validation for signup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 
 const DUMMY_USERS = [
@@ -14,6 +15,10 @@ const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 const signup = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new HttpError("Invalid inputs pasees, please check your data", 422);
+    }
   const { name, email, password } = req.body;
   const hasUser = DUMMY_USERS.find((u) => u.email === email);
   if (hasUser) {
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,11 +1,20 @@
+const { check } = require("express-validator");
 const usersController = require("../controllers/users-controllers");
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 
 router.get("/", usersController.getUsers);
 
-router.post("/signup", usersController.signup);
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersController.signup
+);
 
 router.post("/login", usersController.login);
 
