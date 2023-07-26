60479e7d90a56ee7b0e3b0ea91b531e298744efc
HarunSarii <harunsari14@gmail.com>
2023-07-26 10:15:57 +0300

    feat: create new place using mongoose

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 const getCoordsForAddress = require("../util/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -56,15 +57,32 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  const createdPlace = {
-    id: uuidv4(),
+  console.log("coordinates:", coordinates);
+
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    }, // change static data to dynamic data later!
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/400px-Empire_State_Building_%28aerial_view%29.jpg",
     creator,
-  };
-  DUMMY_PLACES.push(createdPlace);
+  });
+
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, please try again.",
+      err,
+      500
+    );
+    return next(error);
+  }
+
   res.status(201).json({ place: createdPlace });
 };
 
